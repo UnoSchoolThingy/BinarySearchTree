427a21cca773bdf8b85c9f7293aaa8e6d17f8aca
U-BSD\463453 <463453@SHS-HJCBND2.beaverton.k12.or.us>
2023-03-13 12:14:57 -0700

    Did bst except remove thingy

--- BST.h
+++ BST.h
@@ -1,28 +1,72 @@
 #include <cstring>
 
-// Just making this binary search tree a red black tree so I can copy paste into the next assignment
-
-// Looked at https://en.wikipedia.org/wiki/Red%E2%80%93black_tree 
-
-enum class Color : uint8_t {
-  BLACK,
-  RED
-};
+using std::cout;
+using std::endl;
 
 struct Node {
   int data;
-  Node* parent, left, right;
-  Color color;
+  Node* parent, *left, *right;
 
   Node() {
     memset(this, 0x0, sizeof(Node));
+  }
+
+  Node(int data) {
+    memset(this, 0x0, sizeof(Node));
+    this->data = data;
   }
 };
 
 struct BST {
   Node* root;
-
+  
   BST() {
     this->root = nullptr;
   }
+
+  bool empty() {
+    return root == nullptr;
+  }
+
+  void print(Node* n = reinterpret_cast<Node*>(69420), int depth = 0) {
+    if (reinterpret_cast<unsigned long long>(n) == 69420) n = this->root; // terrible fix but im lazy 
+    if (n == nullptr) return;
+    depth++;
+    print(n->right, depth);
+    for (int i = 0; i < depth - 1; i++) {
+      cout << "          ";
+    }
+    cout << n->data << endl;
+    print(n->left, depth);
+  }
+  
+  bool insert(int data) {
+    if (root == nullptr) {
+      root = new Node(data);
+      return true;
+    }
+    Node* prev = nullptr;
+    Node* curr = root;
+    while (curr != nullptr) {
+      prev = curr;
+      if (data > curr->data) curr = curr->right;
+      else if (data < curr->data) curr = curr->left;
+      else return false; // We have a duplicate :(
+    }
+    ((data < prev->data) ? prev->left : prev->right) = new Node(data);
+    return true;
+  }
+
+  bool contains(int data) {
+    // Search for a value
+    Node* curr = root;
+    while (curr != nullptr) {
+      if (curr->data == data) return true;
+      if (data > curr->data) curr = curr->right;
+      else curr = curr->left;
+    }
+    return false;
+  }
+
+  
 };
--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 int main() {
-  // insane code 
+  srand(time(NULL));
+  BST tree;
+  for (int i = 0; i < 10; i++) {
+    while (!tree.insert(rand() % 20 + 1));
+  }
+  tree.print();
+  for (int i = 0; i < 20; i++) {
+    cout << i;
+    if (tree.contains(i)) cout << " CONTAINED!";
+    cout << endl;
+  }
   return 0;
 }
